2979871b9ce2f389d5227bbef7a54c049f9402c4
Eduardo Salinas <107603999+Eduardo-Salinas-ES@users.noreply.github.com>
2023-04-22 21:54:58 -0700

    Added functionality to the main method

--- Program.cs
+++ Program.cs
@@ -10,8 +10,30 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
+            string line;
+            int numOfCards;
+            
 
+            Console.Write("Hello there, Enter a number of cards to generate: ");
 
+            line = Console.ReadLine();
+
+            if(int.TryParse(line, out numOfCards))
+            {
+                // CardPicker.PickSomeCards(numOfCards) returns a string array that contains our cards
+                // so here we display each card in the array of cards
+                foreach (string card in CardPicker.PickSomeCards(numOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("The number you entered is invalid.");
+            }
+
+            Console.ReadLine();
 
 
 
